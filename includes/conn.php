bed91d0159f61bbd577f7c5ecc61c95ef8862cce
Peterson <webbuyrich@gmail.com>
2016-07-07 12:42:52 -0500

    application complete. added file upload button

--- includes/conn.php
+++ includes/conn.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 $user = 'dbo632050929';
 $pass = 'WebDev22#1';
@@ -13,4 +14,19 @@ try {
     echo 'Connection failed: ' . $e->getMessage();
     die();
 }
+=======
+<?php
+$user = 'webdev';
+$pass = 'intranet';
+
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=think_tank', $user, $pass);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+    die();
+}
+>>>>>>> 3d9b5c6c0b5bcff09055d36915593f3609c3327b
 ?>
--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 $(document).ready(function() {
 	$('input[type=text]').val('');
 	$('input[type=email]').val('');
@@ -109,4 +110,44 @@ $(document).ready(function() {
 	});
 	
 	
+=======
+$(document).ready(function() {
+	$('input[type=text]').val('');
+	$('input[type=email]').val('');
+
+	$('#submitBtn').click(function(e){
+		e.preventDefault();		
+		/*var proposal = $('#inputProposal').val();
+		var employee = $('#inputEmployeeName').val();
+		var department = $('#inputDepartment').val();
+		var jobTitle = $('#inputjobTitle').val();
+		var email = $('#inputEmail').val();
+		var reasons = $('#inputReasons').val();
+		var information = $('#inputInformation').val();
+		var costs = $('#inputCosts').val();
+		var diff = $('#inputDiff').val();
+		var resource = $('#inputResource').val();*/
+		
+		$.post("test-file.php",$('#thinkTank').serializeArray(),function(data){
+			var success = '1 row inserted';
+			console.log(data);
+			if(data === success){
+				$('#success-modal').modal('show');
+				$('#success-modal-body').delay(500).show('slow');
+				setTimeout(function(){
+				  $('#thinkTank')[0].reset();
+				}, 2000);
+				
+				$('#success-close').click(function(){
+					$('#success-modal-body').hide('slow');
+				});
+			}
+		});
+
+		
+		
+	});
+	
+	
+>>>>>>> 3d9b5c6c0b5bcff09055d36915593f3609c3327b
 });
